0552991c16dc042e22c03ecaf83d47f5ed3af77b
Luar Roji <luar@roji.net>
2014-10-28 06:29:52 -0200

    Added simple build commands

---
+++ debian/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+revhash=$(git rev-parse --short HEAD)
+revcount=$(git rev-list --count HEAD)
+
+version=$(dpkg-parsechangelog --count 1 |grep 'Version:'|awk '{print $2}')-r${revcount}-${revhash}
+
+debchange -m -D unstable --force-distribution -v $version "Automatic build"
+
+debuild --no-lintian -us -uc -b
+cd ..
+mkdir -p deploy
+mv gate_* deploy
+cd deploy
+dpkg-scanpackages . /dev/null > Packages
+bzip2 -kf Packages
